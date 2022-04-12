d4debd70cba14c8ccca28178aae66d7f6069f2cb
Avrumi Friedman <a.friedman07@gmail.com>
2022-04-11 23:51:39 -0400

    Add project files.

---
+++ EFRelationships.Data/PeopleCarsContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFRelationships.Data
+{
+    public class PeopleCarsContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public PeopleCarsContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<QuestionsTags> QuestionsTags { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //set up composite primary key
+            modelBuilder.Entity<QuestionsTags>()
+                .HasKey(qt => new { qt.QuestionId, qt.TagId });
+           
+        }
+    }
+}
